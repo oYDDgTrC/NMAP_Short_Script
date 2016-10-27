b82164aa896764a06123ac244f0fee50b9d592fc
N3cr0t1cN1nja <hjthomas91@yahoo.com>
2016-10-27 08:37:38 -0400

    Added Baseline Large Networks

--- NMAP_Short_Script.py
+++ NMAP_Short_Script.py
@@ -12,37 +12,41 @@ IP_array = []
 string_array = hosts.split(",")
 for host in string_array:
 	IP_array.append(ipaddress.ip_address(host))
-
-
+# Uses subprocess to call regular CLI commands. Subprocess has no return value by default. 
 def NmapPingTraceroute (IP_List):
 	for IP in IP_List:
-		print (IP)
-		print (subprocess.call(["nmap", "-sn" , "-PO[1]" , IP])) 
-		print (subprocess.call(["nmap", "-sn" , "--traceroute" , IP])) 
-
-def Baseline (IP_List):
+		print ("\n" + IP)
+		subprocess.call(["nmap", "-sn" , "-PO[1]" , IP])
+		subprocess.call(["nmap", "-sn" , "--traceroute" , IP]) 
+		
+def NmapFullPortScan (IP_List):
 	for IP in IP_List:
 		print ("\n" + IP)
-		print (subprocess.call(["nmap", "-Pn", "-p" "U:0-65535,T:0-65535" , "-PO[1]" , IP])) 
+		subprocess.call(["nmap", "-v", "-A", "T5", "-sS", "-sU", "-p0-65535", IP])
+
 def BaselineLargeNetworks(IP_List):
 	for IP in IP_List:
 		print ("\n" + IP)
-		print (subprocess.call(["nmap", "-Pn", "-p T:0-65535", "-sU" "--top-ports 500" , "-PO[1]" , IP])) 
+		subprocess.call(["nmap", "-Pn", "-sU" , "--top-ports 500" , "-PO[1]" , IP])
+		subprocess.call(["nmap", "-Pn", "-sT" , "-PO[1]" , IP])
 
 print ("***Nmap Scan***")
 print ("(1) = Ping / Traceroute\n")
-print ("(2) = Baseline Scan \n")
+print ("(2) = Standard Full Port Scan\n")
 print ("(3) = Baseline Large Networks Scan \n")
-Scan_choice = input("What type of scan would you like to perform? :  ") 
+Scan_choice = input("What type of scan would you like to perform? :  \n") 
 
 if Scan_choice == "1":
 	NmapPingTraceroute(string_array)
-	print ("Have a nice day!!!!")
-elif Scan_choice == "2":
-	Baseline(string_array)
-	print ("Have a nice day!!!!")
+	break
+if Scan_choice == "2":
+	NmapFullPortScan(string_array)
+	break
 elif Scan_choice == "3":
 	BaselineLargeNetworks(string_array)
-	print ("Have a nice day!!!!")
+	break
 else:
 	print ("No choice was made... ")
+
+print ("Have a nice day!!!!")
+	
