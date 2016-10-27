a1673f03caff31e5386bf98ca3b6b8876abd862b
N3cr0t1cN1nja <hjthomas91@yahoo.com>
2016-10-27 08:36:09 -0400

    Added Baseline Scan for Large networks

--- NMAP_Short_Script.py
+++ NMAP_Short_Script.py
@@ -13,28 +13,36 @@ string_array = hosts.split(",")
 for host in string_array:
 	IP_array.append(ipaddress.ip_address(host))
 
+
 def NmapPingTraceroute (IP_List):
 	for IP in IP_List:
 		print (IP)
-		print (subprocess.call(["nmap", "-sn" , "-PO[1]" , "--traceroute" , IP])) 
-		
-def NmapFullPortScan (IP_List):
+		print (subprocess.call(["nmap", "-sn" , "-PO[1]" , IP])) 
+		print (subprocess.call(["nmap", "-sn" , "--traceroute" , IP])) 
+
+def Baseline (IP_List):
 	for IP in IP_List:
-		print (IP)
-		print ((subprocess.call(["nmap", "-v", "-A", "T5", "-sS", "-sU", "-p0-65535", IP])))
+		print ("\n" + IP)
+		print (subprocess.call(["nmap", "-Pn", "-p" "U:0-65535,T:0-65535" , "-PO[1]" , IP])) 
+def BaselineLargeNetworks(IP_List):
+	for IP in IP_List:
+		print ("\n" + IP)
+		print (subprocess.call(["nmap", "-Pn", "-p T:0-65535", "-sU" "--top-ports 500" , "-PO[1]" , IP])) 
 
 print ("***Nmap Scan***")
 print ("(1) = Ping / Traceroute\n")
-print ("(2) = Standard Full Port Scan\n")
-Scan_choice = input("What type of scan would you like to perform? :  \n") 
+print ("(2) = Baseline Scan \n")
+print ("(3) = Baseline Large Networks Scan \n")
+Scan_choice = input("What type of scan would you like to perform? :  ") 
 
 if Scan_choice == "1":
 	NmapPingTraceroute(string_array)
 	print ("Have a nice day!!!!")
-	
-if Scan_choice == "2":
-	NmapFullPortScan(string_array)
-	print ("Have a nice day!!")
+elif Scan_choice == "2":
+	Baseline(string_array)
+	print ("Have a nice day!!!!")
+elif Scan_choice == "3":
+	BaselineLargeNetworks(string_array)
+	print ("Have a nice day!!!!")
 else:
 	print ("No choice was made... ")
-	
